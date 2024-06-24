6739bc3ac28f8ed6f0f4d9a3600d7afc4346de52
Steven Huang <sarsky228@gmail.com>
2024-06-24 22:48:59 +0800

    Finish Adding item to cart feature & cart box display feature

--- script.js
+++ script.js
@@ -7,26 +7,54 @@ const pricePerItem = +document.querySelector('.product__price-now').textContent.
 const priceAmount = document.querySelector('.cart__item--price-amount');
 const priceTotal = document.querySelector('.cart__item--price-total');
 
+const addToCart = document.querySelector('.product__add');
+const cart = document.querySelector('.header__profile--cart');
+const cartBox = document.querySelector('.cart');
+const cartEmpty = document.querySelector('.cart__content--empty');
+const cartItem = document.querySelector('.cart__item');
+const deleteCart = document.querySelector('.cart__item--delete');
+
 let qtyValue = +qty.textContent;
 
-qtyContainer.addEventListener('click', function (e) {
-  // guard clause
-  if (!e.target.closest('.product__qty-btn')) return;
+const cartContent = function () {
+  if (qtyValue === 0) {
+    cartEmpty.classList.remove('hidden');
+    cartItem.classList.add('hidden');
+  } else {
+    cartEmpty.classList.add('hidden');
+    cartItem.classList.remove('hidden');
+  }
+};
 
+qtyContainer.addEventListener('click', function (e) {
   const btn = e.target.closest('.product__qty-btn');
 
-  if (btn === decreaseBtn) {
-    if (qtyValue === 0) {
-      qtyValue = 0;
-    } else {
-      qtyValue--;
-    }
-  }
+  // guard clause
+  if (!btn) return;
 
-  if (btn === increaseBtn) {
-    qtyValue++;
-  }
+  if (btn === decreaseBtn) qtyValue = qtyValue === 0 ? 0 : --qtyValue;
+  if (btn === increaseBtn) ++qtyValue;
+
   qty.textContent = qtyValue;
   priceAmount.textContent = qtyValue;
   priceTotal.textContent = `$${(+pricePerItem * qtyValue).toFixed(2)}`;
+});
+
+addToCart.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (e.target.closest('.product__add') === addToCart) {
+    if (qtyValue === 0) return;
+    cartBox.classList.remove('hidden');
+    cartContent();
+  }
+});
+
+deleteCart.addEventListener('click', function (e) {
+  cartEmpty.classList.remove('hidden');
+  cartItem.classList.add('hidden');
+});
+
+cart.addEventListener('click', function (e) {
+  cartBox.classList.toggle('hidden');
 });
